a6bc0535b0be55da989eb9be2572f6ed3103c576
Andrew Kim <akim618@gmail.com>
2018-11-11 20:50:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,17 +2,17 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
- puts "Hello, my name is ${name}"
+ puts "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(name)
   speakers.each do
     badge_maker(speakers)
-
 end
 
+room_number = [1,2,3,4,5,6,7]
 def assign_rooms(name)
-
+  puts "Hello #{name}, You'll be assigned to room #{room_number}"
 end
 
 def printer()
